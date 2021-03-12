633bf48ca7dfb7f36824850f53dd41f549b01952
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-12 21:03:45 +0100

    adding axis.h and node.h

---
+++ include/node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "point.h"
+
+class node {
+
+public:
+    Point m_point;
+    node* m_left = nullptr;
+    node* m_right = nullptr;
+
+    node();
+
+    node(Point& point, node* ptr_left, node* ptr_right)
+            : m_point(point)
+            , m_left(ptr_left)
+            , m_right(ptr_right)
+    {
+    }
+};
+#endif /* NODE_H */
