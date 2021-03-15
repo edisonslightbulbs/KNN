e0eb60306c977f7425d7ddd405881b553438c700
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-15 16:05:51 +0100

    switching to small letter convention for namespaces

--- src/knn.cpp
+++ src/knn.cpp
@@ -5,7 +5,7 @@
 #include "timer.h"
 #include "logger.h"
 
-std::vector<float> Knn::compute(std::vector<Point>& points)
+std::vector<float> knn::compute(std::vector<Point>& points)
 {
     /** create container for Kth nearest neighbours of all points */
     std::vector<float> knn4;
