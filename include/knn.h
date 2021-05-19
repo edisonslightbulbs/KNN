97f08c0fa23d2e291a5eeacd1ae2602758ff1ae7
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-19 17:05:29 +0200

    adding doc

--- include/knn.h
+++ include/knn.h
@@ -24,8 +24,26 @@ namespace knn {
  *    The indexes of the nearest neighbors from closest
  *    neighbor to furthest neighbour
  * */
-
 std::vector<int> compute(
     std::vector<Point>& points, const Point& queryPoint, const int& k);
+
+/** compute
+ *    Computes the K nearest neighbours of a set of queryPoints.
+ *
+ *  @param points
+ *    Set of 3D points (x, y, z).
+ *
+ *  @param k
+ *    K value.
+ *
+ *  @param queryPoints
+ *    Set of 3D query points (x, y, z).
+ *
+ *  @retval
+ *    The squared distance to the Kth nearest neighbor
+ *    for each query point.
+ * */
+std::vector<float> compute(std::vector<Point>& points,
+    const std::vector<Point>& queryPoints, const int& k);
 }
 #endif /* KNN_H */
